0fa81288181252b58003ee84850bd21e3533fb90
mehrnoor <saheledarya140001@gmail.com>
2025-10-30 17:00:59 +0330

    t4

--- app/kafka_producer.py
+++ app/kafka_producer.py
@@ -2,7 +2,7 @@ from kafka import KafkaProducer
 import json, os
 
 def send_message(data):
-    bootstrap_servers = os.getenv("BOOTSTRAP_SERVERS", "my-cluster-kafka-bootstrap:9092")
+    bootstrap_servers = os.getenv("BOOTSTRAP_SERVERS", "localhost:9092")
     topic = os.getenv("TOPIC_NAME", "csv-ingest")
     producer = KafkaProducer(
         bootstrap_servers=[bootstrap_servers],
--- tests/test_csv_ingestor.py
+++ tests/test_csv_ingestor.py
@@ -1,8 +1,12 @@
-import pytest
-from app.csv_ingestor import ingest_csv
+from unittest.mock import patch
+import app.kafka_producer as kafka_producer
 
-def test_ingest_csv(tmp_path):
+@patch.object(kafka_producer, 'producer')
+def test_ingest_csv(mock_producer, tmp_path):
     test_file = tmp_path / "test.csv"
     test_file.write_text("id,name\n1,Ali\n2,Sara")
+
+    mock_producer.send.return_value = None  # Simulate successful send
     ingest_csv(str(test_file))
-    assert True  # اگر خطایی ندهد یعنی موفق بوده
+
+    assert mock_producer.send.call_count == 2
