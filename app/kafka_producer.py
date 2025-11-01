6305de8bc1b164f9b9cefa9b49bd4cd8290d0913
Mehr-Noor <saheledarya140001@gmail.com>
2025-11-01 19:29:17 +0330

    U1

--- app/kafka_producer.py
+++ app/kafka_producer.py
@@ -2,15 +2,15 @@
 from kafka import KafkaProducer
 import json, os
 
-bootstrap_servers = os.getenv("BOOTSTRAP_SERVERS", "localhost:9092")
-topic_name = os.getenv("TOPIC_NAME", "csv-ingest")
-
-producer = KafkaProducer(
-    bootstrap_servers=[bootstrap_servers],
-    api_version=(2, 8, 0),
-    value_serializer=lambda v: json.dumps(v).encode("utf-8")
-)
-
 def send_message(data):
+    bootstrap_servers = os.getenv("BOOTSTRAP_SERVERS", "localhost:9092")
+    topic_name = os.getenv("TOPIC_NAME", "csv-ingest")
+
+    # producer درون تابع ساخته می‌شود
+    producer = KafkaProducer(
+        bootstrap_servers=[bootstrap_servers],
+        api_version=(2, 8, 0),
+        value_serializer=lambda v: json.dumps(v).encode("utf-8")
+    )
     producer.send(topic_name, data)
     producer.flush()
