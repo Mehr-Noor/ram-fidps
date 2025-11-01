825e21bad0667d9a1354c0da9961481b59f2f0f4
Mehr-Noor <saheledarya140001@gmail.com>
2025-11-01 19:38:04 +0330

    U6

--- app/csv_ingestor.py
+++ app/csv_ingestor.py
@@ -1,8 +1,10 @@
+# csv_ingestor.py
 import pandas as pd
-from app.kafka_producer import send_message
+from app.kafka_producer import send_message, KafkaProducerSingleton
 
-def ingest_csv(file_path):
+def ingest_csv(file_path: str):
     df = pd.read_csv(file_path)
+    producer = KafkaProducerSingleton.get_producer()
     for _, row in df.iterrows():
-        message = row.to_dict()
-        send_message(message)
+        send_message(row.to_dict(), producer=producer)
+    producer.flush()  # فقط یک بار flush در پایان
